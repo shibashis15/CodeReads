8a834af3c4c9e761c9b7c3aa69e549390aa705b3
Shibashis Banerjee <69764940+shibashis15@users.noreply.github.com>
2024-04-22 01:56:39 +0530

    Delete cart-client directory

--- cart-client/src/main/java/com/cartservice/com/util/ProductDoesNotExists.java
+++
@@ -1,7 +0,0 @@
-package com.cartservice.com.util;
-
-public class ProductDoesNotExists extends RuntimeException{
-    public ProductDoesNotExists(String message) {
-        super(message);
-    }
-}
